642908dfeae5c2f0af290b46900cffb722a7548b
Razarac00 <orji.john@outlook.com>
2020-09-18 15:42:49 -0500

    Test partially up

--- MessageBoardio.Test/IntegrationTest1.cs
+++ MessageBoardio.Test/IntegrationTest1.cs
@@ -3,6 +3,7 @@ using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
 using MessageBoardio.MVC;
 using System.Net.Http;
+using System.Net;
 
 namespace MessageBoardio.Test
 {
@@ -16,30 +17,37 @@ namespace MessageBoardio.Test
         }
 
         [Fact]
-        public async void StorePostAndGetByIdMsg()
+        public async void StorePostAndGetByMsg()
         {
             // arrange
-            // string name = "a";
+            string expected = "Test";
             // string address = "a";
             // string jsonToPost = JsonSerializer.Serialize(new GroceryStore { Name = name, Address = address });
-            // var contentToPost = new StringContent(jsonToPost, Encoding.UTF8, MediaTypeNames.Application.Json);
+            var contentToPost = new StringContent(expected);
             HttpClient client = _factory.CreateClient();
 
             // act: post a store
-            // HttpResponseMessage postResponse = await client.PostAsync("/api/store", contentToPost);
+            HttpResponseMessage postResponse = await client.PostAsync("/home/index", contentToPost);
 
             // assert
-            // Assert.Equal(HttpStatusCode.Created, postResponse.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, postResponse.StatusCode);
             // Uri storeLocation = postResponse.Headers.Location;
             // Assert.NotNull(storeLocation);
             // GroceryStore postStore = await Utilities.AssertResponseBodyWellFormedJsonAsync<GroceryStore>(postResponse);
             // Assert.Equal(name, postStore.Name);
             // Assert.Equal(address, postStore.Address);
 
-            // act: get the created store
-            // HttpResponseMessage getResponse = await client.GetAsync(storeLocation);
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+            Assert.Contains(expected, responseString);//responseString);
+
+            // arrange: get the message back
+            // var response = await client.GetAsync("/home/index");
+            // act
+            // response.EnsureSuccessStatusCode();
+            // var responseString = await response.Content.ReadAsStringAsync();
 
             // assert
+
             // Assert.Equal(HttpStatusCode.OK, getResponse.StatusCode);
             // GroceryStore getStore = await Utilities.AssertResponseBodyWellFormedJsonAsync<GroceryStore>(getResponse);
             // Assert.Equal(name, getStore.Name);

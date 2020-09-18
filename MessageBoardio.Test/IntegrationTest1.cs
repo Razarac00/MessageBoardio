accef1a8d27cfb17dfcf55f7d299a2b0dc26c52d
Razarac00 <orji.john@outlook.com>
2020-09-18 12:04:52 -0500

    Added a textbox area for submitting comments and started test setup

--- MessageBoardio.Test/IntegrationTest1.cs
+++ MessageBoardio.Test/IntegrationTest1.cs
@@ -1,15 +1,49 @@
 using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
+using MessageBoardio.MVC;
+using System.Net.Http;
 
 namespace MessageBoardio.Test
 {
-    public class UnitTest1
+    public class ServerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
-        [Fact]
-        public void Test1()
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public ServerTests(WebApplicationFactory<Startup> factory)
         {
-            
+            _factory = factory;
+        }
+
+        [Fact]
+        public async void StorePostAndGetByIdMsg()
+        {
+            // arrange
+            // string name = "a";
+            // string address = "a";
+            // string jsonToPost = JsonSerializer.Serialize(new GroceryStore { Name = name, Address = address });
+            // var contentToPost = new StringContent(jsonToPost, Encoding.UTF8, MediaTypeNames.Application.Json);
+            HttpClient client = _factory.CreateClient();
+
+            // act: post a store
+            // HttpResponseMessage postResponse = await client.PostAsync("/api/store", contentToPost);
+
+            // assert
+            // Assert.Equal(HttpStatusCode.Created, postResponse.StatusCode);
+            // Uri storeLocation = postResponse.Headers.Location;
+            // Assert.NotNull(storeLocation);
+            // GroceryStore postStore = await Utilities.AssertResponseBodyWellFormedJsonAsync<GroceryStore>(postResponse);
+            // Assert.Equal(name, postStore.Name);
+            // Assert.Equal(address, postStore.Address);
+
+            // act: get the created store
+            // HttpResponseMessage getResponse = await client.GetAsync(storeLocation);
+
+            // assert
+            // Assert.Equal(HttpStatusCode.OK, getResponse.StatusCode);
+            // GroceryStore getStore = await Utilities.AssertResponseBodyWellFormedJsonAsync<GroceryStore>(getResponse);
+            // Assert.Equal(name, getStore.Name);
+            // Assert.Equal(address, getStore.Address);
         }
     }
 }
